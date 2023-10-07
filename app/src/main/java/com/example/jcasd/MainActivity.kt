87d895d430a06a64048c93b635005f8b0174f3eb
zaidkamil <xaidmetamorphos@gmail.com>
2023-10-07 15:50:52 +0530

    updated MainActivity

--- app/src/main/java/com/example/jcasd/MainActivity.kt
+++ app/src/main/java/com/example/jcasd/MainActivity.kt
@@ -7,12 +7,14 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.room.Room
 import com.example.jcasd.data.local.AppDatabase
 import com.example.jcasd.ui.AppViewModel
+import com.example.jcasd.ui.Navigation
 import com.example.jcasd.ui.theme.JetpackComposeAppSimpleDatabaseTheme
 
 class MainActivity : ComponentActivity() {
@@ -41,13 +43,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposeAppSimpleDatabaseTheme {
-                // A surface container using the 'background' color from the theme
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    // todo 15.0 add state variable
-                    // todo 15.1 call the screen
+                    val state = viewModel.state.collectAsState().value
+                    Navigation(state = state, onEvent = viewModel::onEvent)
                 }
             }
         }
